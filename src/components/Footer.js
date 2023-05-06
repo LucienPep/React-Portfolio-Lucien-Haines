e4619c53aba0236fbe3b00ff37da8420dc14e0b7
LucienPep <xxyyzzoozz.com>
2023-05-06 14:05:54 +0930

    Fixed files

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/Footer.css';
+
+function Footer() {
+  return (
+    <header className="footer">
+      <h1>Welcome</h1>
+    </header>
+  );
+}
+
+export default Footer;
